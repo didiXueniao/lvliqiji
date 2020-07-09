f3d88a19d5c5114ce7dcd50f59c09afb76d86629
mac <1104078047@qq.com>
2020-07-09 20:24:39 +0800

    swiper
    Signed-off-by: mac <1104078047@qq.com>

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,16 @@
 Page({
   data: {
-    text: '我是首页'
+    text: '我的产品展示',
+    imgUrls: [
+      '/image/huafu.jpg',
+      '/image/huafu1.jpg',
+      '/image/huafu2.jpg',
+    ],
+    circular: false,
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 500,
   },
   onLoad() {},
 });
