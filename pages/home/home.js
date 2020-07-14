7ff88b993289e6709ddc532a88852e3d5a60ccdd
mac <1104078047@qq.com>
2020-07-14 16:10:33 +0800

    add hsbc image

--- pages/home/home.js
+++ pages/home/home.js
@@ -2,6 +2,7 @@ Page({
   data: {
     text: '我的产品展示',
     imgUrls: [
+      'https://www3.services.cn-banking.hsbc.com.cn/PublicContent/mobilez_test/PRD_WD/PL/2.34.0/content/scm/native/creditCards/choice_credit_card.png',
       '/image/huafu.jpg',
       '/image/huafu1.jpg',
       '/image/huafu2.jpg',
