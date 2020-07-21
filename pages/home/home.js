7f39286b9f94d87b05ce8c59b38c40c88c917647
mac <1104078047@qq.com>
2020-07-21 16:29:07 +0800

    user  info

--- pages/home/home.js
+++ pages/home/home.js
@@ -29,20 +29,24 @@ Page({
     hasUserInfo: false,
   },
   onLoad() {},
+ onGetAuthorize() {
+        my.getOpenUserInfo({
+          fail: (error) => {
+            console.error('getAuthUserInfo', error);
+          },
+          success: (res) => {
+            console.log(`userInfo:`, res);
+            let userInfo = JSON.parse(res.response).response // 以下方的报文格式解析两层 response
+            
+            console.log(userInfo)
+            my.alert({
+              content:JSON.stringify(userInfo)
+            });
+          }
+        });
+     
+  },
   onItemClick(ev) {
-    if(this.data.hasUserInfo) {
-      console.log(ev.detail);
-      my.alert({
-        title: '亲',
-        content: `您正在访问${ev.detail.index}号服务`,
-      })
-    } else {
-      my.confirm({
-        title: '温馨提示',
-        content: '您还未登录，请登录',
-        confirmButtonText: '登录',
-        cancelButtonText: '取消',
-        success: () => {
           my.getAuthCode({
             scopes: 'auth_user',//auth_base  auth_zhima
             fail: (error) => {
@@ -62,27 +66,45 @@ Page({
                     title: '获取用户信息成功',
                     content: `尊敬的${userInfo.nickName}会员，您好。`,
                   });
-                  setTimeout(() => {
-                    this.setData({
-                      hasUserInfo: false
-                    })
-                  }, 60000);
                 }
               });
+
+              my.getLocation({
+                success(res) {
+                  console.log(`getLocation:`, res);
+                },
+                fail() {
+                  my.alert({ title: '定位失败' });
+                },
+              })
+              my.getSystemInfo({
+                fail: (error) => {
+                  console.error('getSystemInfo', error);
+                },
+                success: (getSystemInfo) => {
+                  console.log(`getSystemInfo:`, getSystemInfo);
+                }
+              });
+
+              my.getSystemInfoSync({
+                fail: (error) => {
+                  console.error('getSystemInfoSync', error);
+                },
+                success: (getSystemInfoSync) => {
+                  console.log(`getSystemInfoSync:`, getSystemInfoSync);
+                }
+              });
+              //获取手机网络状态
+              my.getNetworkType({
+                fail: (error) => {
+                  console.error('getNetworkType', error);
+                },
+                success: (getLocation) => {
+                  console.log(`getNetworkType:`, getLocation);
+                }
+              });
+
             }
           });
-        }
-      });
-    }
-    
-  },
-  handleTapLogonIn() {
-    if(this.data.hasUserInfo){
-      setTimeout(() => {
-        this.setData({
-          hasUserInfo: false
-        })
-      }, 60000);
-    }
-  }
+        },
 });
